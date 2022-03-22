cb531ac7198796be26acef849ff2499c70577a46
ashishss21 <ashish.sengar18@yahoo.com>
2022-03-22 18:16:51 +0530

    React Router Issue resolved

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Home from "./containers/Home";
+import Home from "./containers/Home/index";
 import SingIn from "./containers/SingIn";
 import SingUp from "./containers/SignUp";
 
@@ -11,9 +11,9 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={Home} />
-          <Route path="/signin" element={SingIn} />
-          <Route path="/signup" element={SingUp} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/signin" element={<SingIn/>} />
+          <Route path="/signup" element={<SingUp/>} />
         </Routes>
       </Router>
     </div>
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () =>{
     return(
         <>
-            <h1>Jedshfizdjglh</h1>
+            <h1>Hello World!!</h1>
         </>
     );
 }
