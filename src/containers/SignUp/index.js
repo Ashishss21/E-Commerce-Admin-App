babb0c93c7df73ccfe379b89d6e740f3757cfc02
ashishss21 <ashish.sengar18@yahoo.com>
2022-03-23 23:25:13 +0530

    Admin SingIn/SingUp Form

--- src/containers/SignUp/index.js
+++ src/containers/SignUp/index.js
@@ -1,11 +1,68 @@
 import React from "react";
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
+import Layout from "../../components/Layout";
+import Input from "../../components/UI/Input";
 
-const SingUp = () =>{
-    return(
-        <>
-        <h1>Signup</h1>
-        </>
-    );
-}
+const SingUp = () => {
+  return (
+    <Layout>
+      <Container
+        style={{
+          border: "2px solid #000",
+          marginTop: "150px",
+          padding: "50px",
+        }}
+      >
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Form>
+              <Row>
+                <Col md={6}>
+                  <Input
+                    label="First Name"
+                    placeholder="First Name"
+                    value=""
+                    type="text"
+                    onChange={() => {}}
+                  />
+                </Col>
+                <Col md={6}>
+                  <Input
+                    label="Last Name"
+                    placeholder="Last Name"
+                    value=""
+                    type="text"
+                    onChange={() => {}}
+                  />
+                </Col>
+              </Row>
+
+              <Input
+                label="Email"
+                placeholder="Enter your email"
+                value=""
+                type="text"
+                onChange={() => {}}
+              />
+
+              <Input
+                label="Password"
+                placeholder="Enter your password"
+                value=""
+                type="password"
+                onChange={() => {}}
+              />
+              <div className="d-grid gap-2 mt-3">
+                <Button variant="outline-info" size="lg" type="submit">
+                  Submit
+                </Button>
+              </div>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  );
+};
 
 export default SingUp;
--- src/containers/SingIn/index.js
+++ src/containers/SingIn/index.js
@@ -1,11 +1,47 @@
 import React from "react";
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
+import Layout from "../../components/Layout";
+import Input from "../../components/UI/Input";
 
-const SingIn = () =>{
-    return(
-        <>
-        SignIn
-        </>
-    );
-}
+const SingIn = () => {
+  return (
+    <Layout>
+      <Container
+        style={{
+          border: "2px solid #000",
+          marginTop: "150px",
+          padding: "50px",
+        }}
+      >
+        <Row style={{ margin: "50px" }}>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Form>
+              <Input
+                label="Email"
+                placeholder="Enter your email"
+                value=""
+                type="text"
+                onChange={() => {}}
+              />
+
+              <Input
+                label="Password"
+                placeholder="Enter your password"
+                value=""
+                type="password"
+                onChange={() => {}}
+              />
+              <div className="d-grid gap-2 mt-3">
+                <Button variant="outline-info" size="lg" type="submit">
+                  Submit
+                </Button>
+              </div>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  );
+};
 
 export default SingIn;
